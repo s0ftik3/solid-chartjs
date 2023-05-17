97f164a77b75ffafc465b959e8bbd318eea14613
ZanzyTHEbar <pyr0ndet0s97@gmail.com>
2023-05-17 13:12:09 +0100

    fix: set App.tsx back to the default

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { render } from 'solid-js/web'
-import App from './AllCharts'
+import App from './App'
 render(() => <App />, document.getElementById('root')!)
